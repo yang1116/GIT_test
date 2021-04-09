cc1256002ec8df714d89295f26cb36fb21d1e2ca
yang <xiaodan_yang@qq.com>
2021-04-09 22:22:09 +0800

    4月9日02

---
+++ test.py
@@ -0,0 +1,16 @@
+#
+# print("大一")
+# print("大二")
+# print("大三")
+# print("大四")
+#
+# print("研一")
+# print("研二")
+# print("研三")
+
+print("a"<"b"<"c")
+
+# dict4 = {[1,2,3]:"estc"}
+# print(dict4)
+a=[1,2,3]
+print(a*3)
